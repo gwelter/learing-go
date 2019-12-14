5e72f3237f5d2cbdea39148c911eac11d1da8603
Guilherme Andre Welter <guilherme.a.welter@gmail.com>
2019-12-14 18:24:18 -0300

    Avançando com chamadas http e loop

--- hello.go
+++ hello.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	exibeIntroducao()
-	exibeMenu()
+	for {
+		exibeMenu()
 
-	comando := leComando()
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando desconhecido")
-		os.Exit(-1)
+		comando := leComando()
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando desconhecido")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -32,6 +35,7 @@ func exibeIntroducao() {
 }
 
 func exibeMenu() {
+	fmt.Println("")
 	fmt.Println("1- Inicial monitoramento")
 	fmt.Println("2- Exibir logs")
 	fmt.Println("0- Sair do programa")
@@ -43,4 +47,15 @@ func leComando() int {
 	fmt.Println("O comando escolhido foi", comando)
 
 	return comando
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://random-status-code.herokuapp.com"
+	response, _ := http.Get(site)
+	if response.StatusCode == 200 {
+		fmt.Println("Site:", site, "carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code: ", response.StatusCode)
+	}
 }
