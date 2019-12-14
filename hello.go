af67580b93b46b99f0eade44f210fa2dd5147deb
Guilherme Andre Welter <guilherme.a.welter@gmail.com>
2019-12-14 17:59:01 -0300

    Progredindo com declaraçao de funçoes

--- hello.go
+++ hello.go
@@ -1,19 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	exibeIntroducao()
+	exibeMenu()
+
+	comando := leComando()
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando desconhecido")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
 	nome := "Guilherme"
 	versao := 1.1
 	fmt.Println("Olá sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("1- Inicial monitoramento")
 	fmt.Println("2- Exibir logs")
 	fmt.Println("0- Sair do programa")
+}
 
+func leComando() int {
 	var comando int
 	fmt.Scan(&comando)
-
 	fmt.Println("O comando escolhido foi", comando)
+
+	return comando
 }
